d8791c6e55fc9d3387339accc3bc16bc8a2382a9
Diluxshana-2001 <139696121+Diluxshana-2001@users.noreply.github.com>
2024-11-19 16:10:50 +0000

    viwe button click added

--- studentapp/src/components/StudentTable.js
+++ studentapp/src/components/StudentTable.js
@@ -1,5 +1,8 @@
 import '../assets/css/compo.css';
+import Profile from './Profile';
+import {useState} from 'react';
 export default function StudentTable(props){
+    const [stu,setStu]=useState(props.students[0]);
     return(
         <div className='outerDiv'>
             <div className='leftDiv'>
@@ -21,13 +24,16 @@ export default function StudentTable(props){
                                     <td>{student.lastName}</td>
                                     <td>{student.course}</td>
                                     <td>{student.address.country}</td>
-                                    <td><button> View</button></td>
+                                    <td><button onClick={()=>setStu(student)}> View</button></td>
                                 </tr>
 
                             )
                         }
                     </tbody>
                 </table>
+                <div className="leftDiv">
+                    <Profile  student={stu} />
+                </div>
             </div>
         </div>
     );
